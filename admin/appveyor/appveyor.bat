f25667d2397a10d89d820d4d3bf949c8cba67d35
sndnvaps <sndnvaps@gmail.com>
2019-08-31 00:24:24 +0800

    try  3: to use Mingw64 to build SQLite3Enc

--- admin/appveyor/appveyor.bat
+++ admin/appveyor/appveyor.bat
@@ -1,3 +1,7 @@
+rem automatically go to particular toolset based in where compiler is by defaults.
+if exist "C:\mingw-w64\x86_64-7.2.0-posix-seh-rt_v5-rev1\mingw32\bin\g++.exe" set TOOLSET=mingw64 && set PATH="C:\mingw-w64\x86_64-7.2.0-posix-seh-rt_v5-rev1\mingw32\bin";%PATH%
+
+:mingw64
 cd build
 echo %INCLUDE%
 echo %LIB%
