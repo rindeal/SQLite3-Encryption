157b6e53228eb0e628b6ab5ba9f40b914de5e0f2
sndnvaps <sndnvaps@gmail.com>
2019-08-31 13:25:55 +0800

    try 26: use vs2017 msbuild to build SQLite3Enc

--- admin/appveyor/appveyor.bat
+++ admin/appveyor/appveyor.bat
@@ -1,4 +1,5 @@
 rem automatically go to particular toolset based in where compiler is by defaults.
+set MSBUILD_LOGGER=/logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
 goto %TOOLSET%
 
 :mingw32
@@ -12,6 +13,11 @@ goto :eof
 cd build
 mkdir obj-gcc
 mingw32-make.exe -f Makefile config=%CONFIGURATIONS% all
+goto :eof
+
+:msbuild
+cd build
+msbuild /m:2 /v:n /p:Platform=%ARCH% /p:Configuration="%CONFIGURATION%" SQLite3Secure_vc15.sln %MSBUILD_LOGGER%
 goto :eof
 
 :error
